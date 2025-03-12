9837dfc5f5c8d7555c48844e0bb1cfdb1b03062f
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-13 00:50:03 +0300

    Review: Functions

---
+++ index.js
@@ -0,0 +1,37 @@
+function whereIsMyDog(name) {
+  return "Where is " + name + "?";
+}
+let name = "Ted";
+console.log(whereIsMyDog(name));
+
+//"Generalization" By Using Parameters And Arguments
+function exerciseByronThePoodle(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+const dogName = "Bryon";
+const dogBreed = "poodle";
+console.log(exerciseByronThePoodle("Rookie", "Chiwawa"));
+console.log(exerciseByronThePoodle("Bryon", "poodle"));
+console.log(exerciseByronThePoodle("Boo", "puggle"));
+
+//Return Values
+function exerciseDog(dogName, dogBreed) {
+  if (weatherToday === "Rainy") {
+    return `${dogName} did not exercise due to rain`;
+  }
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `${dogName} is happy and tired!`;
+}
+const weatherToday = "Rainy";
+const result = exerciseDog("Byron", "poodle");
+console.log(result);
